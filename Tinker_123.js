ae2da9aa1e3e6986161efb1142deb10b4be21e3a
Delieys <Twiiix1k@gmail.com>
2024-01-13 14:17:35 +0500

    asdasd

--- Tinker_123.js
+++ Tinker_123.js
@@ -357,10 +357,7 @@ var Tinker_Spammer;
 				if (menu_RocketSpam.isKeyDown()) {
 					if (rocket && rocket.IsExist() && rocket.CanCast()) {
 						if (myHero.GetHeroesInRadius(1500, Enum.TeamType.TEAM_ENEMY).length > 0)						                       
-                            rocket.CanCast() && rearm.CanCast();
-						else {
-							null;
-						}						         
+                            rocket.CanCast() && rearm.CanCast() || 0					         
                     }
 				}
             }
